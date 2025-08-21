02f78cc73d2606c8ac7d27b11e8e0e74c17479d4
Koray Canut <koray@careerfoundry.com>
2025-08-21 18:01:59 +0200

    first push of the dbt project

--- models/refunds.sql
+++ models/refunds.sql
@@ -9,16 +9,8 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
+select id, financial_status
+from {{ source('my_cases','pergolux_shopify_ext_2') }}
 
 /*
     Uncomment the line below to remove records with null `id` values
